3d6a619169a3ed5f464b757e14771ad0af596b5e
Patrick Burke <denormative@gmail.com>
2017-01-23 06:36:57 +0900

    PopulationPane

--- src/components/StatsPane.vue
+++ src/components/StatsPane.vue
@@ -6,31 +6,31 @@
       <tbody>
         <tr>
           <th scope="row">Resource clicks</th>
-          <td>{{curCiv.resourceClicks | prettyint(settings)}}</td>
+          <td class="number">{{curCiv.resourceClicks | prettyint(settings)}}</td>
         </tr>
         <tr>
           <th scope="row">Total Land</th>
-          <td><span id="totalLand">1000</span></td>
+          <td class="number"><span id="totalLand">1000</span></td>
         </tr>
         <tr>
           <th scope="row">Total Buildings</th>
-          <td><span id="totalBuildings">0</span></td>
+          <td class="number"><span id="totalBuildings">0</span></td>
         </tr>
         <tr>
           <th scope="row">Enemies Slain</th>
-          <td><span data-action="display" data-target="enemySlain">0</span></td>
+          <td class="number"><span data-action="display" data-target="enemySlain">0</span></td>
         </tr>
         <tr>
           <th scope="row">Unburied Corpses</th>
-          <td><span data-action="display" data-target="corpses">0</span></td>
+          <td class="number"><span data-action="display" data-target="corpses">0</span></td>
         </tr>
         <tr id="graveTotal">
           <th scope="row">Unfilled Graves</th>
-          <td><span data-action="display" data-target="grave">0</span></td>
+          <td class="number"><span data-action="display" data-target="grave">0</span></td>
         </tr>
         <tr id="walkGroup">
           <th scope="row">Walk</th>
-          <td><span id="walkStat">0</span> workers per second</td>
+          <td class="number"><span id="walkStat">0</span> workers per second</td>
         </tr>
       </tbody>
     </table>
