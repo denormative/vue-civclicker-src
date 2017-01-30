8a13ecf7b0b510c5dc1f739dc792620c0a50e7cf
Patrick Burke <denormative@gmail.com>
2017-01-30 19:11:51 +0900

    more tickupdates to main

--- src/main.js
+++ src/main.js
@@ -291,17 +291,17 @@ new Vue({
         })
 
         // Timers - routines that do not occur every second
-        window.doMobs()
-        window.doPestControl()
-        window.tickGlory()
-        window.doShades()
-        window.doEsiege(this.civData.esiege, this.civData.fortification)
+        this.doMobs()
+        this.doPestControl()
+        this.tickGlory()
+        this.doShades()
+        this.doEsiege(this.civData.esiege, this.civData.fortification)
         this.doRaid('party', 'player', 'enemy')
 
         // Population-related
-        window.doGraveyards()
-        window.doHealers()
-        window.doCorpses()
+        this.doGraveyards()
+        this.doHealers()
+        this.doCorpses()
         window.doThrone()
         window.tickGrace()
         window.tickWalk()
@@ -526,6 +526,320 @@ new Vue({
         }
         window.adjustMorale(-0.01)
         window.vm.civData.food.owned = 0
+      }
+    },
+    spawnMob(mobObj, numArg) {
+      let num = numArg
+      let numSge = 0
+      let msg = ''
+
+      if (num === undefined) { // By default, base numbers on current window.vm.population
+        const maxMob = ((window.vm.population.current + window.vm.curCiv.zombie.owned) / 50)
+        num = Math.ceil(maxMob * Math.random())
+      }
+
+      if (num === 0) {
+        return num
+      } // Nobody came
+
+      // Human mobs might bring siege engines.
+      if (mobObj.species === 'human') {
+        numSge = Math.floor((Math.random() * num) / 100)
+      }
+
+      mobObj.owned += num
+      window.vm.civData.esiege.owned += numSge
+
+      msg = `${window.prettify(num)} ${mobObj.getQtyName(num)} attacked` // xxx L10N
+      if (numSge > 0) {
+        msg += `, with ${window.prettify(numSge)} ${window.vm.civData.esiege.getQtyName(numSge)}`
+      } // xxx L10N
+      window.gameLog(msg)
+
+      return num
+    },
+    // Some attackers get a damage mod against some defenders
+    getCasualtyMod(attacker, defender) {
+      // Cavalry take 50% more casualties vs infantry
+      if ((defender.combatType === 'cavalry') && (attacker.combatType === 'infantry')) {
+        return 1.50
+      }
+
+      return 1.0 // Otherwise no modifier
+    },
+    doFight(attacker, defender) {
+      if ((attacker.owned <= 0) || (defender.owned <= 0)) {
+        return
+      }
+
+      // Defenses vary depending on whether the player is attacking or defending.
+      const fortMod = (defender.alignment === 'player' ?
+        (window.vm.civData.fortification.owned * window.vm.civData.fortification.efficiency) :
+        (window.vm.civData.efort.owned * window.vm.civData.efort.efficiency))
+      const palisadeMod = ((defender.alignment === 'player') &&
+        (window.vm.civData.palisade.owned)) * window.vm.civData.palisade.efficiency
+
+      // Determine casualties on each side.  Round fractional casualties
+      // probabilistically, and don't inflict more than 100% casualties.
+      const attackerCas = Math.min(attacker.owned,
+        window.rndRound(this.getCasualtyMod(defender, attacker) * defender.owned * defender.efficiency))
+      const defenderCas = Math.min(defender.owned,
+        window.rndRound(this.getCasualtyMod(attacker, defender) * attacker.owned *
+          (attacker.efficiency - palisadeMod) * Math.max(1 - fortMod, 0)))
+
+      attacker.owned -= attackerCas
+      defender.owned -= defenderCas
+
+      // Give player credit for kills.
+      const playerCredit = ((attacker.alignment === 'player') ? defenderCas : // eslint-disable-line no-nested-ternary
+        (defender.alignment === 'player') ? attackerCas : 0)
+
+      // Increments enemies slain, corpses, and piety
+      window.vm.curCiv.enemySlain.owned += playerCredit
+      if (window.vm.civData.throne.owned) {
+        window.vm.civData.throne.count += playerCredit
+      }
+      window.vm.civData.corpses.owned += (attackerCas + defenderCas)
+      if (window.vm.civData.book.owned) {
+        window.vm.civData.piety.owned += (attackerCas + defenderCas) * 10
+      }
+
+      // Updates window.vm.population figures (including total window.vm.population)
+      window.updatePopulation()
+    },
+    doMobs() { // eslint-disable-line no-unused-vars
+      // Checks when mobs will attack
+      // xxx Perhaps this should go after the mobs attack, so we give 1 turn's warning?
+      let mobType
+      let choose
+      if (window.vm.population.current + window.vm.curCiv.zombie.owned > 0) {
+        window.vm.curCiv.attackCounter += 1
+      } // No attacks if deserted.
+      if (window.vm.population.current + window.vm.curCiv.zombie.owned > 0 && window.vm.curCiv.attackCounter > (60 * 5)) { // Minimum 5 minutes
+        if (600 * Math.random() < 1) {
+          window.vm.curCiv.attackCounter = 0
+          // Choose which kind of mob will attack
+          mobType = 'wolf' // Default to wolves
+          if (window.vm.population.current + window.vm.curCiv.zombie.owned >= 10000) {
+            choose = Math.random()
+            if (choose > 0.5) {
+              mobType = 'barbarian'
+            }
+            else if (choose > 0.2) {
+              mobType = 'bandit'
+            }
+          }
+          else if (window.vm.population.current + window.vm.curCiv.zombie.owned >= 1000) {
+            if (Math.random() > 0.5) {
+              mobType = 'bandit'
+            }
+          }
+          this.spawnMob(window.vm.civData[mobType])
+        }
+      }
+
+      // Handling mob attacks
+      window.getCombatants('home', 'enemy').forEach((attacker) => {
+        if (attacker.owned <= 0) {
+          return
+        } // In case the last one was killed in an earlier iteration.
+
+        const defenders = window.getCombatants(attacker.place, 'player')
+        if (!defenders.length) {
+          attacker.onWin()
+          return
+        } // Undefended
+
+        defenders.forEach((defender) => {
+          this.doFight(attacker, defender)
+        }) // FIGHT!
+      })
+    },
+    doPestControl() { // eslint-disable-line no-unused-vars
+      // Decrements the pestControl Timer
+      if (window.vm.civData.pestControl.timer > 0) {
+        window.vm.civData.pestControl.timer -= 1
+      }
+    },
+    tickGlory() { // eslint-disable-line no-unused-vars
+      // Handles the Glory bonus
+      if (window.vm.civData.glory.timer > 0) {
+        document.getElementById('gloryTimer').innerHTML = window.vm.civData.glory.timer-- // eslint-disable-line no-plusplus
+      }
+      else {
+        document.getElementById('gloryGroup').style.display = 'none'
+      }
+    },
+    doShades() { // eslint-disable-line no-unused-vars
+      const defender = window.vm.civData.shade
+      if (defender.owned <= 0) {
+        return
+      }
+
+      // Attack each enemy in turn.
+      window.getCombatants(defender.place, 'enemy').forEach((attacker) => {
+        const num = Math.floor(Math.min((attacker.owned / 4), defender.owned))
+        // xxx Should we give book and throne credit here?
+        defender.owned -= num
+        attacker.owned -= num
+      })
+
+      // Shades fade away even if not killed.
+      defender.owned = Math.max(Math.floor(defender.owned * 0.95), 0)
+    },
+    // Process siege engine attack.
+    // Returns the number of hits.
+    doSiege(siegeObj, targetObj) {
+      let i
+      let hit
+      let hits = 0
+      // Only half can fire every round due to reloading time.
+      // We also allow no more than 2 per defending fortification.
+      const firing = Math.ceil(Math.min(siegeObj.owned / 2, targetObj.owned * 2))
+      for (i = 0; i < firing; ++i) {
+        hit = Math.random()
+        if (hit > 0.95) {
+          siegeObj.owned -= 1
+        } // misfire; destroys itself
+        if (hit >= siegeObj.efficiency) {
+          continue
+        } // miss
+        hits += 1 // hit
+        if (--targetObj.owned <= 0) { // eslint-disable-line no-plusplus
+          break
+        }
+      }
+
+      return hits
+    },
+    // Deals with potentially capturing enemy siege engines.
+    doEsiege(siegeObj, targetObj) { // eslint-disable-line no-unused-vars
+      if (siegeObj.owned <= 0) {
+        return
+      }
+
+      // First check there are enemies there defending them
+      if (!window.getCombatants(siegeObj.place, siegeObj.alignment).length &&
+        window.getCombatants(targetObj.place, targetObj.alignment).length) {
+        // the siege engines are undefended; maybe capture them.
+        if ((targetObj.alignment === 'player') && window.vm.civData.mathematics.owned) { // Can we use them?
+          window.gameLog(`Captured ${window.prettify(siegeObj.owned)} enemy siege engines.`)
+          window.vm.civData.siege.owned += siegeObj.owned // capture them
+        }
+        siegeObj.owned = 0
+      }
+      else if (this.doSiege(siegeObj, targetObj) > 0) {
+        if (targetObj.id === 'fortification') {
+          window.updateRequirements(targetObj)
+          window.gameLog('Enemy siege engine damaged our fortifications')
+        }
+      }
+    },
+    doGraveyards() { // eslint-disable-line no-unused-vars
+      let i
+      if (window.vm.civData.corpses.owned > 0 && window.vm.curCiv.grave.owned > 0) {
+        // Clerics will bury corpses if there are graves to fill and corpses lying around
+        for (i = 0; i < window.vm.civData.cleric.owned; i++) {
+          if (window.vm.civData.corpses.owned > 0 && window.vm.curCiv.grave.owned > 0) {
+            window.vm.civData.corpses.owned -= 1
+            window.vm.curCiv.grave.owned -= 1
+          }
+        }
+        window.updatePopulationUI()
+      }
+    },
+    // Select a sick worker type to cure, with certain priorities
+    getNextPatient() {
+      let i
+      // xxx Need to generalize this list.
+      const jobs = ['healer', 'cleric', 'farmer', 'soldier', 'cavalry', 'labourer',
+        'woodcutter', 'miner', 'tanner', 'blacksmith', 'unemployed',
+      ]
+      for (i = 0; i < jobs.length; ++i) {
+        if (window.vm.civData[jobs[i]].ill > 0) {
+          return jobs[i]
+        }
+      }
+
+      return ''
+    },
+    // Try to heal the specified number of people in the specified job
+    // Makes them sick if the number is negative.
+    heal(job, numArg) {
+      if (!window.isValid(job) || !job) {
+        return 0
+      }
+      let num = (numArg === undefined) ? 1 : numArg
+
+      num = Math.min(num, window.vm.civData[job].ill)
+      num = Math.max(num, -window.vm.civData[job].owned)
+      window.vm.civData[job].ill -= num
+      window.vm.population.totalSick -= num
+      window.vm.civData[job].owned += num
+      window.vm.population.healthy += num
+
+      return num
+    },
+    doHealers() { // eslint-disable-line no-unused-vars
+      let job
+      let numHealed = 0
+      const numHealers = window.vm.civData.healer.owned + (window.vm.civData.cat.owned * (window.vm.civData.companion.owned))
+
+      // How much healing can we do?
+      window.vm.civData.healer.cureCount += (numHealers * window.vm.civData.healer.efficiency * window.vm.curCiv.morale.efficiency)
+
+      // We can't cure more sick people than there are
+      window.vm.civData.healer.cureCount = Math.min(window.vm.civData.healer.cureCount, window.vm.population.totalSick)
+
+      // Cure people until we run out of healing capacity or herbs
+      while (window.vm.civData.healer.cureCount >= 1 && window.vm.civData.herbs.owned >= 1) {
+        job = this.getNextPatient()
+        if (!job) {
+          break
+        }
+        this.heal(job)
+        window.vm.civData.healer.cureCount -= 1
+        window.vm.civData.herbs.owned -= 1
+        numHealed += 1
+      }
+
+      return numHealed
+    },
+    // Selects random workers, transfers them to their Ill variants
+    plague(sickNum) {
+      let actualNum = 0
+      let i
+
+      window.updatePopulation()
+      // Apply in 1-worker groups to spread it out.
+      for (i = 0; i < sickNum; i++) {
+        actualNum += -this.heal(window.randomHealthyWorker(), -1)
+      }
+
+      return actualNum
+    },
+    doCorpses() { // eslint-disable-line no-unused-vars
+      if (window.vm.civData.corpses.owned <= 0) {
+        return
+      }
+
+      // Corpses lying around will occasionally make people sick.
+      // 1-in-50 chance (1-in-100 with feast)
+      const sickChance = 50 * Math.random() * (1 + window.vm.civData.feast.owned)
+      if (sickChance >= 1) {
+        return
+      }
+
+      // Infect up to 1% of the window.vm.population.
+      let num = Math.floor((window.vm.population.current / 100) * Math.random())
+      if (num <= 0) {
+        return
+      }
+
+      num = this.plague(num)
+      if (num > 0) {
+        window.updatePopulation()
+        window.gameLog(`${window.prettify(num)} workers got sick`) // notify player
       }
     },
 
